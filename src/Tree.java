90a8c7f0d020603ed1ac845b69f4c66f294756b2
seppe <seppe.degryse@telenet.be>
2023-10-29 20:36:13 +0100

    Updated Tree.java (inheriting Graph.java)

--- src/Tree.java
+++ src/Tree.java
@@ -1,9 +1,4 @@
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-
-public class Tree<Vertex> implements TreeInterface<Vertex> {
-    private final HashMap<Vertex, HashSet<Vertex>> adjacencyList = new HashMap<>();
+public class Tree<Vertex> extends Graph<Vertex> implements TreeInterface<Vertex> {
 
     /**
      * Adds an edge to the graph.
@@ -12,122 +7,32 @@ public class Tree<Vertex> implements TreeInterface<Vertex> {
      * @param end   the end point of the edge
      * @throws IllegalArgumentException if the edge creates a loop
      */
+    @Override
     public void addEdge(Vertex start, Vertex end) throws IllegalArgumentException {
-        if (!adjacencyList.containsKey(start)) {
-            adjacencyList.put(start, new HashSet<>());
-        }
-        if (!adjacencyList.containsKey(end)) {
-            adjacencyList.put(end, new HashSet<>());
-        }
-
         if (!canHaveAsEdge(start, end)) {
             throw new IllegalArgumentException("No loops allowed!");
         }
 
-        adjacencyList.get(start).add(end);
-        adjacencyList.get(end).add(start);
+        super.addEdge(start, end);
     }
 
+    /**
+     * Returns whether the edge can be added to the graph.
+     *
+     * @param start the starting point of the edge
+     * @param end   the end point of the edge
+     */
     public Boolean canHaveAsEdge(Vertex start, Vertex end) {
         if (start == end) {
             return false;
         }
-        if (!adjacencyList.containsKey(start)) {
+        if (!getVertices().contains(start)) {
             return true;
         }
-        if (!adjacencyList.containsKey(end)) {
-            return true;
+        if (getVertices().contains(end)) {
+            return false;
         }
-        return !adjacencyList.get(start).contains(end);
-    }
-
-    /**
-     * Returns a collection of integers representing the vertices of the graph.
-     */
-    @Override
-    public Collection<Vertex> getVertices() {
-        return adjacencyList.keySet();
-    }
-
-    /**
-     * Returns the number of edges present in the graph.
-     */
-    @Override
-    public int getNumberOfEdges() {
-        int sum = 0;
-        for (HashSet<Vertex> edges : adjacencyList.values()) {
-            sum += edges.size();
-        }
-        return sum / 2;
-    }
-
-    /**
-     * Returns the number of nodes present in the graph.
-     */
-    @Override
-    public int getNumberOfVertices() {
-        return adjacencyList.size();
-    }
-
-    /**
-     * Returns whether node u and node v are neighbours.
-     *
-     * @param u the first vertex
-     * @param v the second vertex
-     */
-    @Override
-    public boolean areNeighbors(Vertex u, Vertex v) {
-        return adjacencyList.get(u).contains(v);
-    }
-
-    /**
-     * Returns the degree of node u.
-     *
-     * @param u the vertex to get the degree of
-     */
-    @Override
-    public int getDegree(Vertex u) {
-        return adjacencyList.get(u).size();
-    }
-
-    /**
-     * Returns a collection of integers representing the neighbours of node u.
-     *
-     * @param u the vertex to get the neighbours of
-     */
-    @Override
-    public Collection<Vertex> getNeighborsOf(Vertex u) {
-        return adjacencyList.get(u);
-    }
-
-    /**
-     * Construct a spanning tree of the graph, which is used as the initial solution for the local search
-     * (metaheuristic) algorithm.
-     */
-    @Override
-    public TreeInterface<Vertex> getInitialSpanningTree() {
-        return null;
-    }
-
-    /**
-     * Apply the local search algorithm. It returns an estimate for the hamiltonian completion number and needs to stop
-     * after maxIterations iterations.
-     *
-     * @param maxIterations the maximum number of iterations
-     */
-    @Override
-    public int applyLocalSearchAlgorithm(int maxIterations) {
-        return 0;
-    }
-
-    /**
-     * Apply your chosen metaheuristic. It returns an estimate for the hamiltonian completion number.
-     *
-     * @param maxIterations the maximum number of iterations
-     */
-    @Override
-    public int applyMetaheuristic(int maxIterations) {
-        return 0;
+        return !getNeighborsOf(start).contains(end);
     }
 
     /**
--- tests/GraphTests.java
+++ tests/GraphTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GraphTests {
     @Test
     public void testAddEdge() {
-        Graph<Integer> graph = new Graph<Integer>();
+        Graph<Integer> graph = new Graph<>();
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(0, 3);
@@ -31,6 +31,39 @@ public class GraphTests {
         assertFalse(graph.areNeighbors(1, 2));
         assertFalse(graph.areNeighbors(1, 3));
         assertFalse(graph.areNeighbors(1, 5));
+    }
+
+    @Test
+    public void testTreeAddEdge(){
+        Tree<Integer> tree = new Tree<>();
+        tree.addEdge(0, 1);
+        tree.addEdge(0, 2);
+        tree.addEdge(0, 3);
+        tree.addEdge(0, 4);
+        tree.addEdge(5, 3);
+        tree.addEdge(6, 5);
+
+        assertEquals(6, tree.getNumberOfEdges());
+        assertEquals(7, tree.getNumberOfVertices());
+        assertTrue(tree.areNeighbors(0, 1));
+        assertTrue(tree.areNeighbors(0, 2));
+        assertTrue(tree.areNeighbors(0, 3));
+        assertTrue(tree.areNeighbors(0, 4));
+        assertFalse(tree.areNeighbors(1, 4));
+        assertFalse(tree.areNeighbors(2, 3));
+        assertFalse(tree.areNeighbors(3, 4));
+        assertTrue(tree.areNeighbors(5, 3));
+        assertFalse(tree.areNeighbors(5, 4));
+        assertFalse(tree.areNeighbors(0, 5));
+        assertFalse(tree.areNeighbors(1, 2));
+        assertFalse(tree.areNeighbors(1, 3));
+        assertFalse(tree.areNeighbors(1, 5));
+
+        assertThrows(IllegalArgumentException.class, () -> tree.addEdge(0, 0));
+        assertThrows(IllegalArgumentException.class, () -> tree.addEdge(2, 3));
+        assertThrows(IllegalArgumentException.class, () -> tree.addEdge(3, 4));
+        assertThrows(IllegalArgumentException.class, () -> tree.addEdge(1, 4));
+        assertThrows(IllegalArgumentException.class, () -> tree.addEdge(5, 4));
     }
 
 }
