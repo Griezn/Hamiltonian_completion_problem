bed12a4188a55e7ecd93c1d92e77b6e68e1f13c3
seppe <seppe.degryse@telenet.be>
2023-12-05 12:45:28 +0100

    New graph

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Graph<Integer> graph = (Graph<Integer>) Constructor.createGraphFromFile("./Benchmarks/testingConnected/circle_like_2000_10.in");
+        Graph<Integer> graph = (Graph<Integer>) Constructor.createGraphFromFile("./Benchmarks/testingConnected/er_13_9.in");
 
         long startTime = System.nanoTime();
-        graph.applyLocalSearchAlgorithm(3000);
+        System.out.println(graph.applyMetaheuristic(30));
 
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
